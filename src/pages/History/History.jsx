e5d5490e5812fe723db3aad89e931efaaa239c3a
fritzhuie <fritzhuie@gmail.com>
2024-01-26 14:52:11 -0500

    add quiz history

--- src/pages/History/History.jsx
+++ src/pages/History/History.jsx
@@ -3,21 +3,47 @@ import { useState, useEffect } from 'react'
 
 const History = () => {
 const [history, setHistory] = useState(null)
+const [selectQuiz, setSelectQuiz] = useState(null)
 
   useEffect(() => {
     const fetchHistory = async () => {
       const historyData = await quizService.getHistory()
+      console.log(historyData)
       setHistory(historyData)
     }
     fetchHistory()
   }, [])
 
+  const showQuiz = (index) => {
+    setSelectQuiz(history[index])
+  }
 
+  const hideQuiz = () => {
+    setSelectQuiz(null)
+  }
+
+  if (!history) {
+    return <h1>Loading history...</h1>
+  }
+
+  if (!selectQuiz) {
+    return (
+      <>
+      {history.map((quiz, index) => {
+        return <h3 key={index} onClick={() => showQuiz(index)}>{quiz.prompt}</h3>
+      })}
+      </>
+    )
+  }
 
   return (
     <>
-    <div>History</div>
-    
+      <h2 className="quiz-prompt">{selectQuiz.prompt}</h2>
+      <p className="answer-button-correct">{selectQuiz.answer}</p>
+      <p className="close-button">{selectQuiz.wrongAnswers[0]}</p>
+      <p className="close-button">{selectQuiz.wrongAnswers[1]}</p>
+      <p className="close-button">{selectQuiz.wrongAnswers[2]}</p>
+      <button onClick={hideQuiz} className="close-button">Close</button>
     </>
     
   )
