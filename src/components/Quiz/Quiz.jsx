9cb43dd6774ab7657b299606e2aa5031111f9666
laurenmarsh <laurenmdb@gmail.com>
2024-01-26 11:22:11 -0500

    added news page to navbar

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -9,13 +9,12 @@ const Quiz = () => {
     answer: "",
     question: ""
   });
-  const [isFeedbackOpen, setIsFeedbackOpen] = useState(false)
+  const [isFeedbackOpen, setIsFeedbackOpen] = useState(false);
 
   // Effect to fetch quiz data on component mount
   useEffect(() => {
     const fetchQuiz = async () => {
       const quiz = await quizService.getQuiz();
-      console.log()
       setQuizData(quiz); // Set fetched quiz data into state
     };
     fetchQuiz();
@@ -23,37 +22,53 @@ const Quiz = () => {
 
   // Conditional rendering based on fetched data
   if (!quizData || !quizData.wrongAnswers) return <div>Loading quiz...</div>; // Loading state
+
   const handleAnswerClick = (answer) => {
     console.log("Selected answer:", answer);
-
-  }
+  };
 
   const handleFeedbackSubmit = (feedback) => {
     console.log('Submitting feedback:', feedback);
     // Here, implement the submission logic, possibly using a service to POST feedback to your backend
     setIsFeedbackOpen(false); // Close modal after submission
   };
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <h2 className="text-2xl font-bold mb-4">Quiz</h2>
-      <p className="text-xl mb-4">{quizData.prompt}</p>
-      <div className="flex flex-col items-center">
-        <button className="mb-2 px-4 py-2 border rounded text-white bg-blue-500 hover:bg-blue-600" onClick={() => handleAnswerClick(quizData.answer)}>
-          {quizData.answer}
-        </button>
-        {quizData.wrongAnswers.map((altAnswer, index) => (
-          <button key={index} className="mb-2 px-4 py-2 border rounded text-white bg-blue-500 hover:bg-blue-600" onClick={() => handleAnswerClick(altAnswer)}>
-            {altAnswer}
-          </button>
-        ))}
-      </div>
 
-      <button className="mt-4 px-4 py-2 border rounded text-blue-500 border-blue-500 hover:bg-blue-500 hover:text-white" onClick={() => setIsFeedbackOpen(true)}>Give Feedback</button>
-      <Feedback
-        isOpen={isFeedbackOpen}
-        onClose={() => setIsFeedbackOpen(false)}
-        onSubmit={handleFeedbackSubmit}
-      />
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="bg-white p-8 rounded-md shadow-md md:w-96 w-full">
+        <h2 className="text-2xl mb-4">What does this sentence mean?</h2>
+        <p className="text-xl mb-4">{quizData.prompt}</p>
+        <div className="flex flex-col items-center">
+          <button
+            className="mb-2 px-4 py-2 border rounded text-white bg-blue-500 hover:bg-blue-600 w-full"
+            onClick={() => handleAnswerClick(quizData.answer)}
+          >
+            {quizData.answer}
+          </button>
+          {quizData.wrongAnswers.map((altAnswer, index) => (
+            <button
+              key={index}
+              className="mb-2 px-4 py-2 border rounded text-white bg-blue-500 hover:bg-blue-600 w-full"
+              onClick={() => handleAnswerClick(altAnswer)}
+            >
+              {altAnswer}
+            </button>
+          ))}
+        </div>
+
+        <button
+          className="mt-4 px-4 py-2 border rounded text-blue-500 border-blue-500 hover:bg-blue-500 hover:text-white w-full"
+          onClick={() => setIsFeedbackOpen(true)}
+        >
+          Give Feedback
+        </button>
+
+        <Feedback
+          isOpen={isFeedbackOpen}
+          onClose={() => setIsFeedbackOpen(false)}
+          onSubmit={handleFeedbackSubmit}
+        />
+      </div>
     </div>
   );
 };
