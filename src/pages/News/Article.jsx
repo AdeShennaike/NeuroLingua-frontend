fb2e0798b96708da132e9d096ae5176930d1f22f
fritzhuie <fritzhuie@gmail.com>
2024-01-27 18:11:31 -0500

    add styling to news

--- src/pages/News/Article.jsx
+++ src/pages/News/Article.jsx
@@ -5,13 +5,14 @@ const Article = (props) => {
   const { content } = props
 
   return (
-    <div className="news-article">
+    <div>
       <NewsContent displayClass="news-headline" content={content.headline} />
       <NewsContent displayClass="news-author" content={content.author} />
       <NewsContent displayClass="news-date" content={content.date} />
       {content.content.map((section, index) => {
         return <NewsContent key={index} displayClass="news-paragraph" content={section} />
       })}
+      <div style={{ height: "100px" }}></div>
     </div>
   )
 }
